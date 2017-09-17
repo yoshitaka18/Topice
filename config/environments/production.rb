20fe2ae6fffe6d8691143cca2a03b81d41edd300
Yoshitaka Kondo <bad.skyshadow@gmail.com>
2017-09-17 14:11:43 +0000

    env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: '13.114.126.198' }
+  config.action_mailer.default_url_options = { host: 'http://13.114.126.198' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
