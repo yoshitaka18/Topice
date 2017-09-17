2278a8659634916544695a4d5c1e5778d634c3f2
Yoshitaka Kondo <bad.skyshadow@gmail.com>
2017-09-17 12:48:21 +0000

    mod upload file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: '13.114.126.198' }
+  config.action_mailer.default_url_options = { host: 'fast-brook-76067.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
