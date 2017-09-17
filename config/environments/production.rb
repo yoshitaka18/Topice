9457134f32c207d4804cec7a815cfa93cfbe8bc9
Yoshitaka Kondo <bad.skyshadow@gmail.com>
2017-09-17 12:12:25 +0000

    mod Mail Env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'floating-lowlands-25784.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: '13.114.126.198' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
