693f93ce4930fb0ca37fa87b7dbb41fea7a2e425
Yoshitaka Kondo <bad.skyshadow@gmail.com>
2017-09-18 06:39:30 +0000

    mod top site

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,7 @@ class Message < ActiveRecord::Base
   belongs_to :conversation
   belongs_to :user
 
+  validates :body,  presence: true
   validates_presence_of :body, :conversation_id, :user_id
   def message_time
     created_at.strftime("%m/%d/%y at %l:%M %p")
